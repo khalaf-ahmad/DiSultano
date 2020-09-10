306090cce6d65eb880cf8da02095d808b027a1d9
Ahmad khalaf <khalaf_ahmad@outlook.com>
2020-09-10 17:44:49 +0300

    add questinModel component

---
+++ frontend/src/components/UI/QuestionModal/QuestionModal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Modal, Button } from 'react-bootstrap';
+
+
+const QuestionModal = (props) => {
+  return (
+    <Modal show={props.show} onHide={props.close_clicked}>
+      <Modal.Header closeButton>
+        <Modal.Title>{props.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>do you want to continue?</Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={props.close_clicked}>
+          no
+        </Button>
+        <Button variant="danger" onClick={props.submit_clicked}>
+          yes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default QuestionModal;
