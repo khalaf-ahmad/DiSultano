722aeea7b27361b37e0503af567122c6b07749ac
Ahmad khalaf <khalaf_ahmad@outlook.com>
2020-09-13 19:12:09 +0300

    update product component

--- frontend/src/components/ProductList/Product/Product.js
+++ frontend/src/components/ProductList/Product/Product.js
@@ -1,6 +1,5 @@
 import React, {useRef, useEffect} from 'react';
-import { Col, Card, Accordion, ListGroup } from 'react-bootstrap';
-import classes from './Product.module.css';
+import { Col, Card } from 'react-bootstrap';
 import axios from "../../../axios-base";
 
 const Product = ({
@@ -25,13 +24,14 @@ const Product = ({
   }, [product.image]);
 
   return (
-    <Col role="button" {...size} onClick={card_clicked} onDoubleClick={on_double_click}>
-      <Card>
+    <Col role="button" {...size} onClick={card_clicked}
+      onDoubleClick={on_double_click}>
+      <Card className='mb-2' >
         {product.image ? (
           <Card.Img
             className="bg-warning"
             variant="top"
-            height="150px"
+            height="100px"
             ref={img_ref}
           />
         ) : null}
@@ -50,31 +50,6 @@ const Product = ({
             {(+product.price.toFixed(2)).toLocaleString()}
           </Card.Text>
         </Card.Body>
-        {product.receivers && product.receivers.length ? (
-          <Accordion
-            role="button"
-            as={Card.Footer}
-            className={classes.CardFooter}
-          >
-            <Accordion.Toggle
-              className="text-warning p-2"
-              as="div"
-              eventKey="0"
-              onClick={(event) => event.stopPropagation()}
-            >
-              Recievers
-            </Accordion.Toggle>
-            <Accordion.Collapse as="div" eventKey="0">
-              <ListGroup className={classes.ListGroup}>
-                {product.receivers.map((user) => (
-                  <ListGroup.Item key={user.id} variant="warning">
-                    {user.name}
-                  </ListGroup.Item>
-                ))}
-              </ListGroup>
-            </Accordion.Collapse>
-          </Accordion>
-        ) : null}
       </Card>
     </Col>
   );
