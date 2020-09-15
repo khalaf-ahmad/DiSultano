5477aacc290ef1c6168b980abde58c973c3a8d34
Ahmad khalaf <khalaf_ahmad@outlook.com>
2020-09-15 21:55:11 +0300

    add order details reducer and start watch actions on it

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,14 @@
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
-import { watchUsers, watchCategories, watchProducts, watchOrders } from './sagas';
+import {
+    watchUsers, watchCategories, watchProducts,
+    watchOrders, watchOrdersDetails
+} from './sagas';
 import usersReducer from "./reducers/users";
 import categoryReducer from "./reducers/category";
 import productReducer from "./reducers/product";
 import orderBuilderReducer from './reducers/order_builder';
-
+import ordersDetails from './reducers/orders_details';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     || compose;
@@ -14,7 +17,8 @@ const rootReducer = combineReducers({
     users: usersReducer,
     category: categoryReducer,
     product: productReducer,
-    order_builder: orderBuilderReducer
+    order_builder: orderBuilderReducer,
+    orders_details: ordersDetails
 });
 
 const sagaMiddleWare = createSagaMiddleware();
@@ -28,5 +32,6 @@ sagaMiddleWare.run(watchUsers);
 sagaMiddleWare.run(watchCategories);
 sagaMiddleWare.run(watchProducts);
 sagaMiddleWare.run(watchOrders);
+sagaMiddleWare.run(watchOrdersDetails)
 
 export default store;
