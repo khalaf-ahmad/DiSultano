7d909bee9e42aee953baeee1154b04e44744119b
Ahmad khalaf <khalaf_ahmad@outlook.com>
2020-09-18 22:22:22 +0300

    move categories state to CategoryList container and make optimization for extra rendering

--- frontend/src/containers/OrderBuilder/CategoryList/CategoryList.js
+++ frontend/src/containers/OrderBuilder/CategoryList/CategoryList.js
@@ -1,35 +1,54 @@
-import React,{ useEffect, useCallback} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import ListGroup from 'react-bootstrap/ListGroup';
-import * as actions from '../../../store/actions';
+import React, { useEffect, useCallback, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ListGroup from "react-bootstrap/ListGroup";
+import * as actions from "../../../store/actions";
 
-// Category list component
-const CategoryList = (props) => {
+const initial_category = { id: 0, name: "", products: [] };
+const get_initial_category = () => ({ ...initial_category });
+
+const CategoryList = ({ category_changed }) => {
+  // Component State
+  const [selected_category, setCategory] = useState(get_initial_category());
 
   const dispatch = useDispatch();
 
+  // Getting categories from redux state
   const categories = useSelector((state) => state.category.categories);
 
+  // Mapp to fetch categories action
   const fetch_categories = useCallback(
     () => dispatch(actions.fetch_categories(true)),
     [dispatch]
+  );
+
+  const on_select_category = useCallback(
+    (category) => setCategory({ ...category }),
+    []
   );
 
   useEffect(() => {
     fetch_categories();
   }, [fetch_categories]);
 
+  useEffect(() => {
+    category_changed(selected_category);
+  }, [category_changed, selected_category]);
+
   return (
     <ListGroup
-      variant="flush" role="button" as="ul" className="text-capitalize">
+      variant="flush"
+      role="button"
+      as="ul"
+      className="text-capitalize"
+    >
       {categories.map((category) => (
         <ListGroup.Item
           as="li"
           className="cursor-pointer"
           action
-          active={category.id === props.selected_category.id}
+          active={category.id === selected_category.id}
           key={category.id}
-          onClick={() => props.category_clicked(category)}
+          onClick={() => on_select_category(category)}
         >
           {category.name}
         </ListGroup.Item>
@@ -38,5 +57,4 @@ const CategoryList = (props) => {
   );
 };
 
-
-export default CategoryList;
+export default React.memo(CategoryList);
