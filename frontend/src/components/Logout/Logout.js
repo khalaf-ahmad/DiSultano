d3dcc203dd90743bb9a61f933fba6204147b4b86
Ahmad Khalaf <khalaf_ahmad@outlook.com>
2020-09-23 04:02:24 +0300

    reset store state when user logout

--- frontend/src/components/Logout/Logout.js
+++ frontend/src/components/Logout/Logout.js
@@ -1,14 +1,19 @@
-import React, { useContext, useEffect } from "react";
-import { Redirect } from 'react-router-dom';
-import AuthContext from '../../context/auth-context';
+import React, { useContext, useEffect, useCallback } from "react";
+import { Redirect } from "react-router-dom";
+import AuthContext from "../../context/auth-context";
+import { useDispatch } from "react-redux";
+import { USER_LOGOUT } from "../../store/actions/actionTypes";
 
 const Logout = () => {
-    const authContext = useContext(AuthContext);
-    useEffect(() => {
-        authContext.resetAuth();
-    }, [authContext]);
+  const authContext = useContext(AuthContext);
+  const dispatch = useDispatch();
+  const logout = useCallback(() => dispatch({ type: USER_LOGOUT }), [dispatch]);
+  useEffect(() => {
+    logout();
+    authContext.resetAuth();
+  }, [authContext, logout]);
 
-    return  <Redirect to="/login" />  ;
-}
+  return <Redirect to="/login" />;
+};
 
 export default Logout;
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,37 +1,47 @@
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import {
-    watchUsers, watchCategories, watchProducts,
-    watchOrders, watchOrdersDetails
-} from './sagas';
+  watchUsers,
+  watchCategories,
+  watchProducts,
+  watchOrders,
+  watchOrdersDetails,
+} from "./sagas";
 import usersReducer from "./reducers/users";
 import categoryReducer from "./reducers/category";
 import productReducer from "./reducers/product";
-import orderBuilderReducer from './reducers/order_builder';
-import ordersDetails from './reducers/orders_details';
+import orderBuilderReducer from "./reducers/order_builder";
+import ordersDetails from "./reducers/orders_details";
+import { USER_LOGOUT } from "./actions/actionTypes";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const rootReducer = combineReducers({
-    users: usersReducer,
-    category: categoryReducer,
-    product: productReducer,
-    order_builder: orderBuilderReducer,
-    orders_details: ordersDetails
+const appReducer = combineReducers({
+  users: usersReducer,
+  category: categoryReducer,
+  product: productReducer,
+  order_builder: orderBuilderReducer,
+  orders_details: ordersDetails,
 });
+
+const rootReducer = (state, action) => {
+  if (action.type === USER_LOGOUT) {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
 
 const sagaMiddleWare = createSagaMiddleware();
 
 const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(sagaMiddleWare))
+  rootReducer,
+  composeEnhancers(applyMiddleware(sagaMiddleWare))
 );
 
 sagaMiddleWare.run(watchUsers);
 sagaMiddleWare.run(watchCategories);
 sagaMiddleWare.run(watchProducts);
 sagaMiddleWare.run(watchOrders);
-sagaMiddleWare.run(watchOrdersDetails)
+sagaMiddleWare.run(watchOrdersDetails);
 
 export default store;
