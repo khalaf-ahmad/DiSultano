677c055a20e17100973da636273f47a2fd5c7a85
Ahmad khalaf <khalaf_ahmad@outlook.com>
2020-09-30 03:40:48 +0300

    add README file

--- backend/run.py
+++ backend/run.py
@@ -4,4 +4,4 @@ from disultano.extensions_init import socketio
 app = create_app()
 
 if __name__ == "__main__":
-  socketio.run(app, host='0.0.0.0',debug=True, port=5000)
+  socketio.run(app, host='0.0.0.0',debug=True)
